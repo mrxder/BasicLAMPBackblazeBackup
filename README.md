5dc6270ae1773224e2c90146ddc4efd8fc949007
root <root@mail.snow.at>
2021-02-01 11:17:29 +0100

    Added logging

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# BasicLAMPBackplazeBackup
+# BasicLAMPBackblazeBackup
+
+This is a basic backup script for a LAMP Server.
+As a backup destination, Backblaze B2 is used.
+
+With this script, it is easy to dump SQL Databases and copy the specified directories.
+
+Because B2 support file versioning, this backup solution makes incremental backups with an adjustable parameter how long different versions of a file survive. 
+
+# Installation
+- Setup the b2 CLI tool as described in the b2 documentation.
+- Configure the backup script with the backup.conf file.
+- Adjust the path in the backup.sh script where the conf file gets sourced to your location.
+- Set up a cron job the execute the script regularly:
+- `20 2 * * * /root/backup_script/./backup.sh`
--- backup.sh
+++ backup.sh
@@ -5,11 +5,19 @@ command -v b2 >/dev/null 2>&1 || { echo >&2 "I require b2 but it's not installed
 # SetConfigLocation
 . /root/backup_script/backup.config
 
+startTimeDate=$(date +%Y-%m-%d_%H-%M)
+logFilePath="${logDir}backup_${startTimeDate}.log"
+
+echo "-------- START BACKUP ---------" > "$logFilePath"
+date >> "$logFilePath"
+echo "-------------------------------" >> "$logFilePath"
+
 # Backup Directories
 for i in "${LOCATIONS[@]}"
 do
    IFS=';' read -ra parts <<< "$i"
-   b2 sync --keepDays $keepDays --replaceNewer ${parts[1]} b2://$B2_Bucket/${parts[0]}
+   echo "Backup directory ${parts[0]}" >> "$logFilePath"
+   b2 sync --keepDays $keepDays --replaceNewer ${parts[1]} b2://$B2_Bucket/${parts[0]} >> "$logFilePath"
 done
 
 # Backup SQL
@@ -18,13 +26,20 @@ cd tempSqlBackup
 
 for i in "${DATABASES[@]}"
 do
+   echo "Dump DB $i" >> "$logFilePath"
    mysqldump -u "$DB_USER" -p"$DB_PW" "$i" | gzip -c > "$i.sql.gz"
 done
 
 cd ..
 
-b2 sync --keepDays $keepDays --replaceNewer tempSqlBackup/ b2://$B2_Bucket/sql_db
+b2 sync --keepDays $keepDays --replaceNewer tempSqlBackup/ b2://$B2_Bucket/sql_db >> "$logFilePath"
 
 rm -r tempSqlBackup
 
+echo "--------- END BACKUP ----------" >> "$logFilePath"
+date >> "$logFilePath"
+echo "-------------------------------" >> "$logFilePath"
 
+# Compress Log File
+cat $logFilePath | gzip -c > "${logFilePath}.gz"
+rm $logFilePath
